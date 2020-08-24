101223919a7bac679d2993d78cec9f16e5ae0179
y_fujitani <y_fujitani@appresearch.co.jp>
2020-08-24 19:22:38 +0900

    Add bxslider

--- js/main.js
+++ js/main.js
@@ -143,17 +143,16 @@
 	});
 
 	{
-		
-	 const images = [
-		"url(images/keyvisual.JPG)",
-		"url(images/photographic_portrait.JPG)",
-		"url(images/portfolio_apartment_entrance.JPG)",
-	 ];
-	 let currentIndex =0;
 
-	const main = document.getElementById('main');
-	main.style.backgroundImage = images[currentIndex];
+	// TODO
+	$(document).ready(function () {
+		$('#bxslider').bxSlider({
+			mode: 'fade',
+			auto: 'auto',
+		});
+	});
 	
+
 }
 
 }());
